bd85ba1f654321b54d1d277f3543f0d0005a93c3
Danny Baumann <dannybaumann@web.de>
2014-11-12 17:14:27 -0800

    Add back blacklist settings. [Squashed]
    Cm originally had this squashed, but with the mess that is cm-13 even theyre squashed became unsquashed and you had to dig in like 10 commits that had nothing to do with blacklist to find all the code. thats what happens when one person merges fwbase and another setting.. c'mon man organize your shit!
    
    Change-Id: I45d6c90bd584d65c83504a0fdca96f69f4ee7d94
    Signed-off-by: AudioGod <audiogod@sungsonic.co>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -17,6 +17,7 @@
 package com.android.settings;
 
 import com.android.settings.applications.AppOpsSummary;
+import com.android.settings.blacklist.BlacklistSettings;
 
 /**
  * Top-level Settings activity
@@ -121,4 +122,5 @@ public class Settings extends SettingsActivity {
 	public static class SSHDTunerActivity extends SettingsActivity { /* empty */ }
 	public static class About extends SettingsActivity { /* empty */ }
     public static class ExpandedDesktopSettingsActivity extends SettingsActivity { /* empty */ }
+    public static class BlacklistSettingsActivity extends SettingsActivity { /* empty */ }
 }
