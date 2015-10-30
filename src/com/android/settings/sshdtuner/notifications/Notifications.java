b9bd526a4f45723418bffec211dbcb6d4a8f0f38
TheMrcool212 <munirmohamadrani@gmail.com>
2015-10-30 04:11:27 +0000

    DT2S Navbar (2/2)
    Change-Id: Idb1072ee9cd6613520511165fb392320047880af
    Signed-off-by: AudioGod <audiogod@sungsonic.co>

--- src/com/android/settings/sshdtuner/notifications/Notifications.java
+++ src/com/android/settings/sshdtuner/notifications/Notifications.java
@@ -17,15 +17,41 @@
 package com.android.settings.sshdtuner.notifications;
 
 import android.app.ActivityManagerNative;
+import com.android.internal.view.RotationPolicy;
+import com.android.settings.DropDownPreference.Callback;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.search.Indexable;
+import android.app.Activity;
+import android.app.ActivityManagerNative;
+import android.app.Dialog;
+import android.app.UiModeManager;
+import android.app.admin.DevicePolicyManager;
 import android.content.Context;
 import android.content.ContentResolver;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.database.Cursor;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.os.RemoteException;
 import android.os.ServiceManager;
+import android.os.SystemProperties;
+import android.preference.CheckBoxPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceClickListener;
+import android.preference.PreferenceScreen;
+import android.preference.PreferenceCategory;
+import android.preference.SwitchPreference;
+import android.provider.SearchIndexableResource;
 import android.preference.Preference;
 import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
@@ -37,11 +63,14 @@ import android.view.WindowManagerGlobal;
 import android.view.IWindowManager;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.ArrayList;
+import java.util.List;
 
 import java.util.Locale;
 import android.text.TextUtils;
 import android.view.View;
 import com.android.settings.sshdtuner.seekbar.SeekBarPreference;
+import com.android.internal.util.omni.PackageUtils;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -54,24 +83,99 @@ public class Notifications extends SettingsPreferenceFragment implements
     private static final String PRE_QUICK_PULLDOWN = "quick_pulldown";
 	private static final String PREF_QS_TRANSPARENT_HEADER = "qs_transparent_header";
 	private static final String PREF_QS_TRANSPARENT_SHADE = "qs_transparent_shade";
+	private static final String CUSTOM_HEADER_IMAGE = "status_bar_custom_header";
+    private static final String DAYLIGHT_HEADER_PACK = "daylight_header_pack";
+    private static final String DEFAULT_HEADER_PACKAGE = "com.android.systemui";
+    private static final String CATEGORY_WEATHER = "weather_category";
+    private static final String WEATHER_ICON_PACK = "weather_icon_pack";
+    private static final String STATUS_BAR_HEADER_WEATHER = "status_bar_header_weather";
+    private static final String DEFAULT_WEATHER_ICON_PACKAGE = "org.omnirom.omnijaws";
+    private static final String WEATHER_SERVICE_PACKAGE = "org.omnirom.omnijaws";
+    private static final String LOCK_CLOCK_PACKAGE="com.cyanogenmod.lockclock";
+
+    private ListPreference mDaylightHeaderPack;
+    private CheckBoxPreference mCustomHeaderImage;
+    private PreferenceCategory mWeatherCategory;
+    private ListPreference mWeatherIconPack;
+    private CheckBoxPreference mHeaderWeather;
 
     private ListPreference mQuickPulldown;
 	private SeekBarPreference mQSHeaderAlpha; 
     private SeekBarPreference mQSShadeAlpha;
 	
     private final Configuration mCurConfig = new Configuration();
+	
+	@Override
+    protected int getMetricsCategory() {
+        return MetricsLogger.SSHDTUNER;
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+		final Activity activity = getActivity();
+        final ContentResolver resolver = activity.getContentResolver();
 
         addPreferencesFromResource(R.xml.sshd_notifications);
-        PreferenceScreen prefSet = getPreferenceScreen();
+		
+		final PreferenceScreen prefScreen = getPreferenceScreen();
+        mWeatherCategory = (PreferenceCategory) prefScreen.findPreference(CATEGORY_WEATHER);
+        if (mWeatherCategory != null && !isOmniJawsServiceInstalled()) {
+            prefScreen.removePreference(mWeatherCategory);
+        } else {
+            String settingHeaderPackage = Settings.System.getString(getContentResolver(),
+                    Settings.System.STATUS_BAR_WEATHER_ICON_PACK);
+            if (settingHeaderPackage == null) {
+                settingHeaderPackage = DEFAULT_WEATHER_ICON_PACKAGE;
+            }
+            mWeatherIconPack = (ListPreference) findPreference(WEATHER_ICON_PACK);
+            mWeatherIconPack.setEntries(getAvailableWeatherIconPacksEntries());
+            mWeatherIconPack.setEntryValues(getAvailableWeatherIconPacksValues());
 
-        ContentResolver resolver = getActivity().getContentResolver();
+            int valueIndex = mWeatherIconPack.findIndexOfValue(settingHeaderPackage);
+            if (valueIndex == -1) {
+                // no longer found
+                settingHeaderPackage = DEFAULT_WEATHER_ICON_PACKAGE;
+                Settings.System.putString(getContentResolver(),
+                        Settings.System.STATUS_BAR_WEATHER_ICON_PACK, settingHeaderPackage);
+                valueIndex = mWeatherIconPack.findIndexOfValue(settingHeaderPackage);
+            }
+            mWeatherIconPack.setValueIndex(valueIndex >= 0 ? valueIndex : 0);
+            mWeatherIconPack.setSummary(mWeatherIconPack.getEntry());
+            mWeatherIconPack.setOnPreferenceChangeListener(this);
+
+            mHeaderWeather = (CheckBoxPreference) findPreference(STATUS_BAR_HEADER_WEATHER);
+        }
+
+        final boolean customHeaderImage = Settings.System.getInt(getContentResolver(),
+                Settings.System.STATUS_BAR_CUSTOM_HEADER, 0) == 1;
+        mCustomHeaderImage = (CheckBoxPreference) findPreference(CUSTOM_HEADER_IMAGE);
+        mCustomHeaderImage.setChecked(customHeaderImage);
+
+        String settingHeaderPackage = Settings.System.getString(getContentResolver(),
+                Settings.System.STATUS_BAR_DAYLIGHT_HEADER_PACK);
+        if (settingHeaderPackage == null) {
+            settingHeaderPackage = DEFAULT_HEADER_PACKAGE;
+        }
+        mDaylightHeaderPack = (ListPreference) findPreference(DAYLIGHT_HEADER_PACK);
+        mDaylightHeaderPack.setEntries(getAvailableHeaderPacksEntries());
+        mDaylightHeaderPack.setEntryValues(getAvailableHeaderPacksValues());
+
+        int valueIndex = mDaylightHeaderPack.findIndexOfValue(settingHeaderPackage);
+        if (valueIndex == -1) {
+            // no longer found
+            settingHeaderPackage = DEFAULT_HEADER_PACKAGE;
+            Settings.System.putString(getContentResolver(),
+                    Settings.System.STATUS_BAR_DAYLIGHT_HEADER_PACK, settingHeaderPackage);
+            valueIndex = mDaylightHeaderPack.findIndexOfValue(settingHeaderPackage);
+        }
+        mDaylightHeaderPack.setValueIndex(valueIndex >= 0 ? valueIndex : 0);
+        mDaylightHeaderPack.setSummary(mDaylightHeaderPack.getEntry());
+        mDaylightHeaderPack.setOnPreferenceChangeListener(this);
+        mDaylightHeaderPack.setEnabled(customHeaderImage);
 
         // Quick pulldown
-	mQuickPulldown = (ListPreference) findPreference(PRE_QUICK_PULLDOWN);
+	    mQuickPulldown = (ListPreference) findPreference(PRE_QUICK_PULLDOWN);
         mQuickPulldown.setOnPreferenceChangeListener(this);
         int statusQuickPulldown = Settings.System.getInt(getContentResolver(),
                 Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN, 1);
@@ -79,6 +183,7 @@ public class Notifications extends SettingsPreferenceFragment implements
         updateQuickPulldownSummary(statusQuickPulldown);
 		
 		// QS header alpha
+		PreferenceScreen prefSet = getPreferenceScreen();
         mQSHeaderAlpha =
                 (SeekBarPreference) prefSet.findPreference(PREF_QS_TRANSPARENT_HEADER);
         int qSHeaderAlpha = Settings.System.getInt(resolver,
@@ -87,6 +192,7 @@ public class Notifications extends SettingsPreferenceFragment implements
         mQSHeaderAlpha.setOnPreferenceChangeListener(this);
 		
 		// QS shade alpha
+		PreferenceScreen prefSet = getPreferenceScreen();
         mQSShadeAlpha =
                 (SeekBarPreference) prefSet.findPreference(PREF_QS_TRANSPARENT_SHADE);
         int qSShadeAlpha = Settings.System.getInt(resolver,
@@ -96,13 +202,9 @@ public class Notifications extends SettingsPreferenceFragment implements
     }
 
     @Override
-    protected int getMetricsCategory() {
-        return MetricsLogger.SSHDTUNER;
-    }
-
-    @Override
     public void onResume() {
         super.onResume();
+	updateWeatherSettings();	
     }
 
     @Override
@@ -112,12 +214,33 @@ public class Notifications extends SettingsPreferenceFragment implements
 
     @Override
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+		if (preference == mCustomHeaderImage) {
+            final boolean value = ((CheckBoxPreference)preference).isChecked();
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.STATUS_BAR_CUSTOM_HEADER, value ? 1 : 0);
+            mDaylightHeaderPack.setEnabled(value);
+            return true;
+        }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         final String key = preference.getKey();
-        if (preference == mQuickPulldown) {
+		ContentResolver resolver = getContentResolver();
+           if (preference == mDaylightHeaderPack) {
+            String value = (String) newValue;
+            Settings.System.putString(getContentResolver(),
+                    Settings.System.STATUS_BAR_DAYLIGHT_HEADER_PACK, value);
+            int valueIndex = mDaylightHeaderPack.findIndexOfValue(value);
+            mDaylightHeaderPack.setSummary(mDaylightHeaderPack.getEntries()[valueIndex]);
+        }
+        if (preference == mWeatherIconPack) {
+            String value = (String) newValue;
+            Settings.System.putString(getContentResolver(),
+                    Settings.System.STATUS_BAR_WEATHER_ICON_PACK, value);
+            int valueIndex = mWeatherIconPack.findIndexOfValue(value);
+            mWeatherIconPack.setSummary(mWeatherIconPack.getEntries()[valueIndex]);
+		if (preference == mQuickPulldown) {
             int statusQuickPulldown = Integer.valueOf((String) newValue);
             Settings.System.putInt(getContentResolver(),
                     Settings.System.STATUS_BAR_QUICK_QS_PULLDOWN,
@@ -138,6 +261,63 @@ public class Notifications extends SettingsPreferenceFragment implements
          return false;
     }
 
+	    private boolean isOmniJawsEnabled() {
+        final Uri SETTINGS_URI
+            = Uri.parse("content://org.omnirom.omnijaws.provider/settings");
+
+        final String[] SETTINGS_PROJECTION = new String[] {
+            "enabled"
+        };
+
+        final Cursor c = getContentResolver().query(SETTINGS_URI, SETTINGS_PROJECTION,
+                null, null, null);
+        if (c != null) {
+            int count = c.getCount();
+            if (count == 1) {
+                c.moveToPosition(0);
+                boolean enabled = c.getInt(0) == 1;
+                return enabled;
+            }
+        }
+        return true;
+    }
+
+    private String[] getAvailableHeaderPacksValues() {
+        List<String> headerPacks = new ArrayList<String>();
+        Intent i = new Intent();
+        PackageManager packageManager = getPackageManager();
+        i.setAction("org.omnirom.DaylightHeaderPack");
+        for (ResolveInfo r : packageManager.queryIntentActivities(i, 0)) {
+            String packageName = r.activityInfo.packageName;
+            if (packageName.equals(DEFAULT_HEADER_PACKAGE)) {
+                headerPacks.add(0, packageName);
+            } else {
+                headerPacks.add(packageName);
+            }
+        }
+        return headerPacks.toArray(new String[headerPacks.size()]);
+    }
+
+    private String[] getAvailableHeaderPacksEntries() {
+        List<String> headerPacks = new ArrayList<String>();
+        Intent i = new Intent();
+        PackageManager packageManager = getPackageManager();
+        i.setAction("org.omnirom.DaylightHeaderPack");
+        for (ResolveInfo r : packageManager.queryIntentActivities(i, 0)) {
+            String packageName = r.activityInfo.packageName;
+            String label = r.activityInfo.loadLabel(getPackageManager()).toString();
+            if (label == null) {
+                label = r.activityInfo.packageName;
+            }
+            if (packageName.equals(DEFAULT_HEADER_PACKAGE)) {
+                headerPacks.add(0, label);
+            } else {
+                headerPacks.add(label);
+            }
+        }
+        return headerPacks.toArray(new String[headerPacks.size()]);
+    }
+	
     private void updateQuickPulldownSummary(int value) {
         Resources res = getResources();
 
